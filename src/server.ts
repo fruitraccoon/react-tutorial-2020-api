2e54cbc07217255540bb6326b03087a39a780899
Jonathan Job <jonathan.job@purple.telstra.com>
2020-07-15 11:28:15 +1000

    Add CORS

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import { api } from './api';
 
 const app = express();
 const port = 5000;
+
+app.use(cors({ origin: '*' }));
 
 app.get('/', (req, res) => res.json('The PurPoll API Server is awake!'));
 
