6b105f54a20becd0811856d08493493decc7a2b0
Jonathan Job <jonathan.job@purple.telstra.com>
2020-07-15 10:40:11 +1000

    Add src folder and api module

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { api } from './api';
 
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('The PurPoll API Server is awake!'));
+
+app.use('/api', api);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
